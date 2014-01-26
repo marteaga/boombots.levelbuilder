b03a037a5082be0fca2b79a83c80c93608f35119
Mark Arteaga <info@markarteaga.com>
2014-01-26 09:00:15 -0500

    udpated block to be clonable, updated main window to have right size

--- src/Boombots.LevelEditor/Models/Blocks.cs
+++ src/Boombots.LevelEditor/Models/Blocks.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Boombots.LevelEditor.Models
 {
@@ -12,6 +13,16 @@ namespace Boombots.LevelEditor.Models
 
         public string CharCode { get; set; }
 
-        public System.Windows.Point CurrentCoord { get; set; }
+        public Point CurrentCoord { get; set; }
+
+        public GameBlock Clone()
+        {
+            return new GameBlock()
+            {
+                Filename = this.Filename,
+                CharCode = this.CharCode,
+                CurrentCoord = this.CurrentCoord,
+            };
+        }
     }
 }

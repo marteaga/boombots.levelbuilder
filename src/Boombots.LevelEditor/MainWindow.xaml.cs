5fb2e6171670893c20fbd8baaa0203a2352d385d
Mark Arteaga <info@markarteaga.com>
2014-01-25 22:50:07 -0500

    initial commit with layout

---
+++ src/Boombots.LevelEditor/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Boombots.LevelEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Boombots.LevelEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // board game is 30x16
+
+        private List<GameBlock> _blocks;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CreateBlocksDataSource();
+            lstBlocks.ItemsSource = _blocks;
+
+            // wire up the save button
+            btnSave.Click += btnSave_Click;
+        }
+
+        void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO");
+        }
+
+        private void CreateBlocksDataSource()
+        {
+            _blocks = new List<GameBlock>()
+            {
+                new GameBlock() { Filename = "Images\\blocks\\blue.png",      CharCode = "1", CurrentCoord = new Point(0,0) },
+                new GameBlock() { Filename = "Images\\blocks\\green.png",     CharCode = "2", CurrentCoord = new Point(0,0) },
+                new GameBlock() { Filename = "Images\\blocks\\red.png",       CharCode = "3", CurrentCoord = new Point(0,0) },
+                new GameBlock() { Filename = "Images\\blocks\\yellow.png",    CharCode = "4", CurrentCoord = new Point(0,0) },
+            };
+        }
+    }
+}
---
+++ src/Boombots.LevelEditor/Models/Blocks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boombots.LevelEditor.Models
+{
+    public class GameBlock
+    {
+        public string Filename { get; set; }
+
+        public string CharCode { get; set; }
+
+        public System.Windows.Point CurrentCoord { get; set; }
+    }
+}
